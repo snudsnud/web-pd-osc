23f075c720a7d458dc04004d5b8576bc1e7886a6
Yichen Wang <yichenwang@Yichens-MacBook-Pro.local>
2024-02-09 15:01:39 +1100

    made changes to fit holo ar system websocket data trasmission

--- browser/sketch.js
+++ browser/sketch.js
@@ -1,6 +1,6 @@
 // Set up osc.js with websocket
 var oscPort = new osc.WebSocketPort({
-    url: "ws://localhost:8081", // URL to your Web Socket server.
+    url: "ws://localhost:3000", // URL to your Web Socket server.
     metadata: true
 });
 oscPort.open();
