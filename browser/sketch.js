3d7e5f122db78ec33dff196546d4ce7bf024a84a
snudsnud <sandy.ma.14@outlook.com>
2024-08-29 14:14:11 +1000

    updates to 28/08/2024

--- browser/sketch.js
+++ browser/sketch.js
@@ -1,6 +1,16 @@
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
+
+let video;
+let object = '';
+let confidence = 0;
+let vowels = 0;
+let consonants = 0;
+
+
 // Set up osc.js with websocket
 var oscPort = new osc.WebSocketPort({
-    url: "ws://localhost:3000", // URL to your Web Socket server.
+    url: "ws://localhost:8081", // URL to your Web Socket server.
     metadata: true
 });
 oscPort.open();
@@ -11,7 +21,7 @@ oscPort.on("message", function (oscMsg) {
 // When you need to send oscMsg to Pd via node.js bridge
 function sendOsc(msg)
 {
-    console.log("sending");
+    // console.log("sending");
     oscPort.send({
         address: "/carrier/frequency",
         args: [
@@ -25,12 +35,62 @@ function sendOsc(msg)
 }
 
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    video = createCapture(VIDEO);
+    video.hide();
+    background(0);
+    classifier = ml5.imageClassifier('MobileNet', video, modelReady);
 }
 
 
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+    // Display error in the console
+    if (error) {
+      console.error(error);
+    } else {
+    //   console.log(results);
+      object = results[0].label;
+      confidence = results[0].confidence;
+  
+      modelReady();
+    }
+}
+  
+function modelReady() {
+  classifier.predict(gotResult)
+}
+
+function isVowel(c) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
+}
+
+function checkObject(o) {
+    for (let i = 0; i < o.length; i++) {
+        if (isVowel(o[i])){
+            vowels++;
+        } else {
+            consonants++;
+        }
+    };
+}
+
 function draw() {
-    setInterval(sendOsc(1, 1000));
+    background(0);
+    image(video, 100, 100);
+    fill(255);
+    textSize(32);
+    text(object, 10, height-100);
+    text(confidence, 10, height-50);
+    checkObject(object);
+
+    setInterval(sendOsc(255, 1000));
+    setInterval(sendOsc(vowels, 1000));
+    setInterval(sendOsc(consonants, 1000));
+    setInterval(sendOsc(confidence, 1000));
+
+    vowels = 0;
+    consonants = 0;
 }
 
  
--- node-server/index.js
+++ node-server/index.js
@@ -2,13 +2,6 @@ var osc = require("osc");
     WebSocket = require("ws");
 const express = require('express');
 
-// HoloLens IPs
-const sockets = {};
-const ethernet1 = "::ffff:192.168.0.19";
-const ethernet2 = "::ffff:192.168.0.248";
-var hl3 = ethernet1;
-var hl4 = ethernet2;
-
 // Importing events
 const EventEmitter = require('events');
 // Initializing event emitter instances 
@@ -56,7 +49,7 @@ udpPort.open();
 // Create an Express-based Web Socket
 // and serve up a directory of static files.
 var app = express(),
-    server = app.listen(3000);
+    server = app.listen(8081);
 app.use("/", express.static(__dirname + "/static"));
 // Listen for Web Socket requests.
 var wss = new WebSocket.Server({
@@ -64,17 +57,11 @@ var wss = new WebSocket.Server({
 });
 
 // Listen for Web Socket connections.
-wss.on("connection", function (ws, request, socket) {
+wss.on("connection", function (socket) {
     var socketPort = new osc.WebSocketPort({
         socket: socket,
         metadata: true
     });
-
-    const user_id = request.socket.remoteAddress;
-    ws.id = user_id;
-    sockets[ws.id] = ws;
-    //console.log(ws);
-
 
     socketPort.on("message", function (oscMsg) {
         console.log("An OSC Message was received!", oscMsg);
@@ -87,60 +74,4 @@ wss.on("connection", function (ws, request, socket) {
     //     raw: true
     // });
 
-    ws.on('error', console.error);
-    ws.on('message', function message (data) {
-        console.log(data);
-        to(hl3, data);
-        // if (exp_con == "rmi") {
-        //       //RMI condition
-        //   if (user_id == laptop1 ) {
-        //         to(hl4, data)
-        //   }else if (user_id == laptop2) {
-        //         to(hl3, data)
-        //   }
-        
-        // }else if (exp_con == "bmi") {
-              //BMI condition
-        // if (user_id == hl3) {
-        //   to(hl4, data);
-        // }else if (user_id == hl4) {
-        //   to(hl3, data);
-        // }
-        // }else if (exp_con == "localtest"){
-        //   to("::ffff:127.0.0.1", data)
-        // }else if (exp_con == "hltest"){
-        //   to(hl3, data)
-        // }else if (exp_con == "dostest"){
-        //   to("::ffff:192.168.0.216" ,data)
-        // }else if  (exp_con == "duplex"){
-        // //console.log(data)
-        //   if (user_id == laptop1) {
-        //   to(hl4, data)
-        //   }else if (user_id == laptop2) {
-        //   to(hl3, data)
-        //   }
-    
-        //   if (user_id == hl4) {
-        //     to(hl3, data)
-        //   }else if (user_id == hl3) {
-        //     to(hl4, data)
-        //   }
-    
-        // }
-    
-    
-    
-      });
-
-    ws.on('close', function() {
-        console.log("client left.");
-    });
 });
-
-
-
-// thanks to https://stackoverflow.com/questions/51316727/how-do-i-send-a-message-to-a-specific-user-in-ws-library.
-function to(user, data) {
-    if(sockets[user] && sockets[user].readyState === WebSocket.OPEN)
-        sockets[user].send(data);
-}
